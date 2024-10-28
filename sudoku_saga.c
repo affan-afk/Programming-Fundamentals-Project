e477d60aff3347efecd505743bed78c4931604eb
Muhammad Affan Rasheed <affanrasheed2751@gmail.com>
2024-10-28 19:17:27 +0500

    Update sudoku_saga.c

--- sudoku_saga.c
+++ sudoku_saga.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int user_board[9][9] = {0}; //global 2-D Array containing the elements of user's sudoku board.
+
+void print_board(char x)
+{
+  switch (x) 
+{
+case 'u': //prints user_board.
+  for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            printf("%d ", user_board[i][j]);
+        }
+        printf("\n");
+    }
+{
+}  
